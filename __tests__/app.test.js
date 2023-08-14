dbfa02e144ab8458da5b740df3df0634c35fa373
Pedro Ferreira <prgf2011@gmail.com>
2023-08-14 11:27:42 +0100

    create app.js and app.test.js

---
+++ __tests__/app.test.js
@@ -0,0 +1,7 @@
+const app = require('../app');
+const request = require('supertest');
+const db = require('../db/connection');
+const seed = require('../db/seeds/seed');
+const data = require('../db/data/test-data/');
+
+beforeEach(() => {});
