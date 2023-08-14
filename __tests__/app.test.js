90dc7eaffa9c568b47bfe93bd68aed70e66f9d52
Pedro Ferreira <prgf2011@gmail.com>
2023-08-14 12:14:10 +0100

    first commit on task 2

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,4 +4,41 @@ const db = require('../db/connection');
 const seed = require('../db/seeds/seed');
 const data = require('../db/data/test-data/');
 
-beforeEach(() => {});
+beforeEach(() => {
+  return seed(data);
+});
+
+afterAll(() => {
+  return db.end();
+});
+
+describe('app()', () => {
+  describe('/api/topics', () => {
+    it('200: return with a status of 200', () => {
+      return request(app).get('/api/topics').expect(200);
+    });
+    it('200: return with a status of 200 and a message of Okay', () => {
+      return request(app)
+        .get('/api/topics')
+        .expect(200)
+        .then(({ body }) => {
+          expect(body.msg).toBe('Okay');
+        });
+    });
+    it('200: return with a status of 200 and a message of Okay and a body will all the results from the topics tdatabase', () => {
+      return request(app)
+        .get('/api/topics')
+        .expect(200)
+        .then(({ body }) => {
+          const { topics } = body;
+          topics.forEach((topic) => {
+            expect(topic).toHaveProperty('slug', expect.any(String));
+            expect(topic).toHaveProperty('description', expect.any(String));
+          });
+        });
+    });
+    it('404: returns with a 404 error when making a request to an api that does not exist', () => {
+      return request(app).get('/api/toothpicks').expect(404);
+    });
+  });
+});
--- app.js
+++ app.js
@@ -1,5 +1,23 @@
+const { getTopics } = require('./controllers/app.controllers');
+
 const express = require('express');
 
 const app = express();
 
-app.get('/api/topics');
+app.get('/api/topics', getTopics);
+
+app.use((err, request, response, next) => {
+  if (err.status) {
+    response.status(err.status).send({ msg: err.msg });
+  } else {
+    next(err);
+  }
+});
+
+app.use((err, request, response, next) => {
+  console.log(err);
+  response.status(500).send({ err });
+  next();
+});
+
+module.exports = app;
