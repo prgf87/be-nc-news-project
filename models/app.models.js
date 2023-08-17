8262380f0f860eba4b29c260461eb70e29cc2b42
Pedro Ferreira <prgf2011@gmail.com>
2023-08-17 16:38:00 +0100

    fetch article by id should be protected from injection

--- models/app.models.js
+++ models/app.models.js
@@ -21,10 +21,11 @@ const fetchArticleById = (id) => {
       SELECT articles.*, COUNT(comments.article_id) AS comment_count 
       FROM articles 
       JOIN comments ON comments.article_id = articles.article_id
-      WHERE articles.article_id = ${id}
+      WHERE articles.article_id = $1
       GROUP BY articles.article_id
 
-      `
+      `,
+      [id]
     )
     .then(({ rows }) => {
       const article = rows[0];
