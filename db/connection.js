de3e874b8e2f3d3716186912d6e709809f81c511
Pedro Ferreira <prgf2011@gmail.com>
2025-01-30 14:18:12 +0000

    new db string

--- db/connection.js
+++ db/connection.js
@@ -6,14 +6,14 @@ require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-  throw new Error("PGDATABASE or DATABASE_URL not set");
-}
+// if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+//   throw new Error("PGDATABASE or DATABASE_URL not set");
+// }
 
 const config =
   ENV === "production"
     ? {
-        connectionString: process.env.DATABASE_URL,
+        connectionString: process.env.CONNECTION_STRING,
         max: 2,
       }
     : {};
